3350724a2c92f2782cbb395afaafd222759d1212
Stanislas Laurent <stnsls.lrt.accnts@gmail.com>
2026-05-10 02:52:57 +0200

    fix(infra): remove stale warnings.py and test_warnings.py left behind by merge

--- src/raitap/utils/__init__.py
+++ src/raitap/utils/__init__.py
@@ -5,6 +5,5 @@ from __future__ import annotations
 from raitap.utils.diagnostics import Diagnostic
 from raitap.utils.log import raitap_log
 from raitap.utils.serialization import to_json_serialisable
-from raitap.utils.warnings import suppress_warning
 
 __all__ = ["Diagnostic", "raitap_log", "to_json_serialisable"]
