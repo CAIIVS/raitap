d03f48a369a2b058878ea505317f919a8dd42ddc
Stanislas Laurent <stnsls.lrt.accnts@gmail.com>
2026-07-08 15:56:03 +0200

    refactor(config): drop unused resolve_target prefix helper (refs #301)

--- src/raitap/configs/__init__.py
+++ src/raitap/configs/__init__.py
@@ -1,11 +1,10 @@
 from .schema import AppConfig
-from .utils import cfg_to_dict, register_configs, resolve_run_dir, resolve_target, set_output_root
+from .utils import cfg_to_dict, register_configs, resolve_run_dir, set_output_root
 
 __all__ = [
     "AppConfig",
     "cfg_to_dict",
     "register_configs",
     "resolve_run_dir",
-    "resolve_target",
     "set_output_root",
 ]
